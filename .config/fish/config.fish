14260b340adc6b4efc2eb21781262c70d6c3be61
dvvvxx <dotmarcodestefani@gmail.com>
2020-05-12 15:05:12 +0200

    .profile no more

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,22 @@ if test -z $DISPLAY
   exec startx
 end
 
-source $HOME/.profile 
+# Android Studio on Tiling WM
+set -U _JAVA_AWT_WM_NONREPARENTING 1
+
+# Brew
+if test /home/linuxbrew/.linuxbrew/bin/brew
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
+
+# Go
+if test /usr/local/lib/go/bin
+  #and type -q go # Check if command exists
+  set GOPATH "$HOME/go"
+  set PATH "$PATH:/usr/local/lib/go/bin:$GOPATH/bin"
+end
+
+# Flutter
+if test /usr/local/flutter/bin
+  set PATH "$PATH:/usr/local/flutter/bin"
+end
